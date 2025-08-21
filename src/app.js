c6c6cd2302e7d2b33b2ccab7b805b0de62730bbf
Christopher Riley <rileyc@gmail.com>
2025-08-21 12:10:04 -0700

    improve app error handling

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ if (import.meta.url.startsWith('file:')) {
     const modulePath = url.fileURLToPath(import.meta.url);
     if (process.argv[1] === modulePath) {
         try {
-            const envMap = GenerateFromFile(process.argv[2]);
+            const configFile = process.argv[2];
+            if (configFile == undefined || configFile.trim().length == 0) {
+                throw new Error(`config file was not specified`)
+            }
+
+            const envMap = GenerateFromFile(configFile);
             envMap.forEach((v, k) => {
                 console.log(`${k}=${v}`)
             });
