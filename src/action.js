614138d4d26a8f182c6f2a56936b07aad61c5a6d
Christopher Riley <rileyc@gmail.com>
2025-08-21 10:07:03 -0700

    add github action

---
+++ src/action.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+import * as core from "@actions/core";
+
+try {
+    const GITHUB_ENV = process.env.GITHUB_ENV.trim();
+    if (GITHUB_ENV.length == 0) {
+        core.warning(`WARNING: GITHUB_ENV is not set - this will be a dry run only`);
+    } else {
+        core.info(`GITHUB_ENV: ${GITHUB_ENV}`);
+    }
+
+    const configFile = core.getInput("config-file").trim();
+    if (configFile.length == 0) {
+        throw new Error(`config-file input variable is not set`);
+    }
+    core.info(`config file: ${configFile}`);
+
+    const envMap = GenerateFromFile(configFile);
+    envMap.forEach((v, k) => {
+        core.info(`${k}="${v}"`);
+
+        if (GITHUB_ENV.length > 0) {
+            try {
+                fs.appendFile(GITHUB_ENV, `${k}="${v}"`);
+            } catch (err) {
+                throw new Error(`failed to write to GITHUB_ENV: ${err}`);
+            }
+        }
+    });
+} catch (error) {
+    core.setFailed(error.message);
+}
