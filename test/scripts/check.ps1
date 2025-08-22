5a1764358677ebc899f153f39185af8389504425
Christopher Riley <rileyc@gmail.com>
2025-08-22 14:45:43 -0700

    fix powershell test script

--- test/scripts/check.ps1
+++ test/scripts/check.ps1
@@ -4,11 +4,11 @@ function check {
     $actual = Get-Item Env:$envvarname -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Value
     Write-Output "TEST: `$$envvarname - expected: $expected, actual: $actual"
 
-    if ($expected -ne $actual) {
+    if (($expected.Length -gt 0 -or $actual.Length -gt 0) -and $expected -ne $actual) {
         Write-Output "test failed: `$$envvarname"
         Write-Output ""
-        Write-Output "   expected: $expected"
-        Write-Output "     actual: $actual"
+        Write-Output "   expected: '$expected'"
+        Write-Output "     actual: '$actual'"
         exit 1
     }
 }
