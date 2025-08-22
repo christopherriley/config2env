93b8ba52d9de3fabccd4acdee8535fd30628540c
Christopher Riley <rileyc@gmail.com>
2025-08-22 13:03:13 -0700

    add windows tests

---
+++ test/scripts/check.ps1
@@ -0,0 +1,14 @@
+function check {
+    $envvarname = $args[0]
+    $expected = $args[1]
+    $actual = Get-Variable -Name $envvarname -ValueOnly -ErrorAction SilentlyContinue
+    Write-Output "TEST: `$$envvarname - expected: $expected, actual: $actual"
+
+    if ($expected -ne $actual) {
+        Write-Output "test failed: `$$envvarname"
+        Write-Output ""
+        Write-Output "   expected: $expected"
+        Write-Output "     actual: $actual"
+        exit 1
+    }
+}
